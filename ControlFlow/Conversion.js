0d3ad706a5ab8b80bfc045b367cf0f43a803f150
musaibdeveloper <syedmusaib428@gmail.com>
2023-07-29 15:24:44 +0000

    Folder added

--- ControlFlow/Conversion.js
+++ ControlFlow/Conversion.js
@@ -15,9 +15,9 @@
 // "1"	          "1"	                1	          true
 // "0"	          "0"	                0	          true
 // "ten"	      "ten"	                NaN	          true
-// true	       "   true"	                1	          true
+// true	       "   true"	            1	       true
 // false	       "false"	            0	          false
-// null	            "null"	                0	           false
+// null	            "null"	              0	       false
 // undefined	   "undefined"	        NaN	           false
 // ''	              ""	             0	           false
 // ' '	              " "	             0	            true
