a9c3f43d4797d1ce2a5c6a2c8e09c5672a095324
musaibdeveloper <syedmusaib428@gmail.com>
2023-08-05 15:29:37 +0000

    Folder assigned

---
+++  1. JS Intoduction/ 02 Varaiable and Constants.js
@@ -0,0 +1,51 @@
+// var 
+// let 
+// const
+
+// There are four ways to declare javscript variables, 
+
+/*
+// 1. Let 
+2. Var 
+3. const 
+4. Using nothing by default it takes var 
+
+*/
+
+// var x = 3 
+// console.log(x);
+// //   In var you can redeclare the value.
+// var x =5 
+// console.log(x);
+
+// let x = 3; 
+// console.log(x);
+
+// let x = 5;
+
+
+// var a = 3;
+// console.log(a);
+// a = 5;
+// console.log(a);
+
+// The above technique is reassigned, the vaue can be updated. 
+
+// let a = 3;
+// console.log(a);
+// a =5; 
+// console.log(a);
+
+// let can be reassigned but not redeclare. 
+
+// const a =3 ;
+// console.log(a);
+// const a =7; 
+// console.log(a);
+
+// redeclare can't be done
+
+// const a =4 
+// console.log(a);
+// a= 8989
+// console.log(a);
---
+++  1. JS Intoduction/05 Comments.js
@@ -0,0 +1,18 @@
+// /*
+// JavaScript comments are hints that a programmer can add to make their code easier to read and understand. 
+// They are completely ignored by JavaScript engines.
+// */
+
+// // There are two ways to add comments to code:
+
+// // - Single Line Comments
+// // /* */ There are two ways to add comments to code:
+
+// // - Single Line Comments
+// /*-Multi-line Comments */ 
+
+// Single Line Comments
+// In JavaScript, any line that starts with // is a single
+
+/* Multi-line Comments
+In Javascript, any text between /* and */ //is a multi-line comment. */
