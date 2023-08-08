b4632a45312820c85ac94eb44b8a4377183d9426
musaibdeveloper <syedmusaib428@gmail.com>
2023-08-08 14:56:36 +0000

    Functions

--- 3.  JS Functions/Functions.js
+++ 3.  JS Functions/Functions.js
@@ -1,2 +1,47 @@
 // JAVASCRIPT FUNCTIONS. 
 
+/*
+A function is a block of code that performs a specific task.
+Function is used to run the codes of line with define task.
+Synatx : function name() {    
+}
+
+*/
+
+
+// Example
+
+function name(){
+    console.log("Musaibdeveloper");
+}
+
+// Calling the function.
+
+name();
+
+// The above code is a normal example of function.
+
+// 2. 
+function Greeting (){
+    console.log("Hello!, I hope you are going Good :)");
+}
+
+// Calling the function. 1
+
+
+Greeting();
+
+// The above is a simple example of function
+
+
+// Benefits of Using a Function
+// Function makes the code reusable. You can declare it once and use it multiple times.
+// Function makes the program easier as each small task is divided into a function.
+// Function increases readability.
+
+
+// 2. Function Parameters:
+
+
+
+
