75846327e7960f7c753d4e3c004441ed96c6adf9
musaibdeveloper <syedmusaib428@gmail.com>
2023-08-09 09:34:50 +0000

    Functiuons Updated

--- 3.  JS Functions/1. Funtions.js
+++ 3.  JS Functions/1. Funtions.js
@@ -40,8 +40,46 @@ Greeting();
 // Function increases readability.
 
 
+// Function with Parameters. 
+
+// A function can also be declared with parameters. A parameter is a value that is passed when declaring a function.
+
+function add (a,b){
+   console.log(a*b/a+b);
+}
+ add(4145 , 5484);
+
+// The function the parameters are set and the code is executed when the function is called.
+
+// When the function is called the values are given at calling paranthesis.
 
 
+
+// Function Return. 
+
+// The return statement can be used to return the value to a function call.
+
+// The return statement denotes that the function has ended. Any code after return is not executed.
+
+// If nothing is returned, the function returns an undefined value.
+
+
+function adding(a,b) {
+    return a+b;
+    console.log();
+    }
+    let number1 = +prompt("ENter the number : ")
+    let number2 = +prompt("ENTER THE NUMBER : ")
+    
+    
+    let result = adding(number1 , number2);
+    
+    console.log(result);
+
+
+    // The above code returns the value of the function and store it in the varaiable "result"
+
+    // The further code is not executed after the retuen statement.
 
 
 
